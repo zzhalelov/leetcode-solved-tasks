1ac853b8474621d465e380c4d7e3cb1feeb720bf
zzhalelov <zzhalelov@gmail.com>
2023-05-30 16:22:18 +0600

    .

---
+++ dayOfWeek1185/src/Main.java
@@ -0,0 +1,9 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        s.dayOfTheWeek(01, 06, 2023);
+        System.out.println(s.dayOfTheWeek(30,05,2023));
+    }
+}
